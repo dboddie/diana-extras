a132c714ab880bca3271c5f0462d7c51e29ec5a5
David Boddie <david.boddie@met.no>
2015-09-10 19:17:26 +0200

    Moved schema-related classes into a separate file.

--- LLF_to_KML/llf2kml.py
+++ LLF_to_KML/llf2kml.py
@@ -17,123 +17,18 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import json, pprint, sys
-import schema
+import llf_schema
 
-class LLF_Error(Exception):
-    pass
+def read_file(file_name):
 
-from PyQt4.QtCore import QDateTime, QPointF
-
-class Time:
-
-    def __init__(self, format):
-        self.format = format
-
-    def validate(self, value):
+    d = json.JSONDecoder()
+    data = open(file_name, "rb").read()
     
-        dateTime = QDateTime.fromString(value, self.format)
-        if dateTime.isValid():
-            return dateTime
-        else:
-            raise ValueError
-
-class ShortDate(Time):
-
-    def __init__(self):
-        Time.__init__(self, "yyMMdd")
-
-    def validate(self, value):
-
-        dateTime = Time.validate(self, value)
-        return dateTime.addYears(100)
-
-class Hour(Time):
-
-    def __init__(self):
-        Time.__init__(self, "HH")
-
-    def validate(self, value):
-
-        return Time.validate(self, value).time().hour()
-
-class LonLat:
-
-    def validate(self, value):
+    # Try to decode the data. This raises a ValueError if it fails.
+    llf = d.decode(data)
     
-        if len(value) != 2:
-            raise ValueError
-        elif not -180.0 <= value[0] <= 180.0:
-            raise ValueError
-        elif not -90.0 <= value[1] <= 90.0:
-            raise ValueError
-        else:
-            return QPointF(value[0], value[1])
-
-ISODate = Time("yyyy-MM-ddTHH:mm:ss.zzzZ")
-
-class LLF_File:
-
-    schema = {
-        "header": {
-            "status": unicode,
-            "group":  unicode,
-            "locale": unicode,
-            "ref":    Hour(),
-            "start":  Hour(),
-            "date":   ShortDate(),
-            "end":    Hour(),
-            "type":   unicode,
-            "areas":  [unicode]
-            },
-        "timesteps": [
-            {
-                "range":    [int],
-                "valid":    [ISODate],
-                "forecast": {
-                    "type": "FeatureCollection",
-                    "features": [
-                        {
-                            "geometry": {
-                                "type": "Polygon",
-                                "coordinates": [
-                                    [LonLat()]
-                                    ]
-                                },
-                            "type": "Feature",
-                            "properties": {
-                                "timeStep": ISODate,
-                                "refTime": ISODate,
-                                "parameterGroup": unicode,
-                                "valid": {
-                                    "to": ISODate,
-                                    "from": ISODate
-                                    },
-                                "parameters": { # Do not validate - depends on product type.
-                                    }
-                                },
-                        }]
-                    }
-            }]
-        }
+    return llf_schema.File().validate(llf)
     
-    def __init__(self, file_name = None):
-
-        if file_name:
-            self.read(file_name)
-
-    def read(self, file_name):
-    
-        d = json.JSONDecoder()
-        try:
-            data = open(file_name, "rb").read()
-        except IOError:
-            raise LLF_Error, "Failed to read file: %s" % file_name
-        
-        # Try to decode the data. This raises a ValueError if it fails.
-        llf = d.decode(data)
-        
-        self.contents = schema.validate(llf, LLF_File.schema)
-
 
 if __name__ == "__main__":
 
@@ -144,5 +39,9 @@ if __name__ == "__main__":
     
     geojson_file, kml_file = sys.argv[1:3]
 
-    llf = LLF_File(geojson_file)
-    pprint.pprint(llf.contents)
+    llf = read_file(geojson_file)
+    
+    for timestep in llf:
+        print timestep['valid']
+        for feature in timestep['forecast']:
+            print feature
---
+++ LLF_to_KML/llf_schema.py
@@ -0,0 +1,176 @@
+
+# Copyright (C) 2015 MET Norway
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import schema
+
+class LLF_Error(Exception):
+    pass
+
+from PyQt4.QtCore import QDateTime, QPointF
+
+class Time:
+
+    def __init__(self, format):
+        self.format = format
+
+    def validate(self, value):
+    
+        dateTime = QDateTime.fromString(value, self.format)
+        if dateTime.isValid():
+            return dateTime
+        else:
+            raise ValueError
+
+class ShortDate(Time):
+
+    def __init__(self):
+        Time.__init__(self, "yyMMdd")
+
+    def validate(self, value):
+
+        dateTime = Time.validate(self, value)
+        return dateTime.addYears(100)
+
+class Hour(Time):
+
+    def __init__(self):
+        Time.__init__(self, "HH")
+
+    def validate(self, value):
+
+        return Time.validate(self, value).time().hour()
+
+class LonLat:
+
+    def validate(self, value):
+    
+        if len(value) != 2:
+            raise ValueError
+        elif not -180.0 <= value[0] <= 180.0:
+            raise ValueError
+        elif not -90.0 <= value[1] <= 90.0:
+            raise ValueError
+        else:
+            return QPointF(value[0], value[1])
+
+ISODate = Time("yyyy-MM-ddTHH:mm:ss.zzzZ")
+
+class Feature:
+
+    schema = {
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [LonLat()]
+                ]
+            },
+        "type": "Feature",
+        "properties": {
+            "timeStep": ISODate,
+            "refTime": ISODate,
+            "parameterGroup": unicode,
+            "valid": {
+                "to": ISODate,
+                "from": ISODate
+                },
+            "parameters": { # Do not validate - depends on product type.
+                }
+            },
+    }
+    
+    def validate(self, value):
+
+        return LLF_Feature(schema.validate(value, Feature.schema))
+
+class Forecast:
+
+    schema = {
+        "type": "FeatureCollection",
+        "features": [Feature()]
+        }
+
+    def validate(self, value):
+
+        return LLF_Forecast(schema.validate(value, Forecast.schema))
+
+class File:
+
+    schema = {
+        "header": {
+            "status": unicode,
+            "group":  unicode,
+            "locale": unicode,
+            "ref":    Hour(),
+            "start":  Hour(),
+            "date":   ShortDate(),
+            "end":    Hour(),
+            "type":   unicode,
+            "areas":  [unicode]
+            },
+        "timesteps": [
+            {
+                "range":    [int],
+                "valid":    [ISODate],
+                "forecast": Forecast()
+            }]
+        }
+
+    def validate(self, value):
+
+        return LLF_File(schema.validate(value, File.schema))
+
+# Containers for validated data
+
+class LLF_Element:
+
+    def __init__(self, contents):
+
+        self.contents = contents
+
+    def keys(self):
+
+        return self.contents.keys()
+
+    def __getitem__(self, key):
+
+        return self.contents[key]
+
+class LLF_Container(LLF_Element):
+
+    def __getitem__(self, index):
+    
+        if type(index) == int:
+            return self.contents[self.container_attr][index]
+        else:
+            return LLF_Element.__getitem__(self, index)
+    
+    def __len__(self):
+
+        return len(self.contents[self.container_attr])
+
+class LLF_Feature(LLF_Element):
+
+    pass
+
+class LLF_Forecast(LLF_Container):
+
+    container_attr = "features"
+
+class LLF_File(LLF_Container):
+
+    container_attr = "timesteps"
+
