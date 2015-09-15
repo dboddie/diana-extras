71fb39424ca87a766e93c775fc4f303e1864e449
David Boddie <david.boddie@met.no>
2015-09-15 15:06:54 +0200

    Made the script perform a simple conversion to KML.

--- LLF_to_KML/llf2kml.py
+++ LLF_to_KML/llf2kml.py
@@ -17,7 +17,10 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import json, pprint, sys
+from xml.etree.ElementTree import Element, ElementTree, SubElement
 import llf_schema
+
+from PyQt4.QtCore import Qt
 
 def read_file(file_name):
 
@@ -40,8 +43,56 @@ if __name__ == "__main__":
     geojson_file, kml_file = sys.argv[1:3]
 
     llf = read_file(geojson_file)
+
+    kml = Element('kml')
+    kml.set('xmlns', "http://www.opengis.net/kml/2.2")
+    doc = SubElement(kml, 'Document')
     
+    # Obtain information about each timestep in the file.
     for timestep in llf:
-        print timestep['valid']
+    
         for feature in timestep['forecast']:
-            print feature
+        
+            folder = SubElement(doc, 'Folder')
+            name = SubElement(folder, 'name')
+            name.text = 'xxx'
+            valid = timestep['valid']
+            
+            timespan = SubElement(folder, 'TimeSpan')
+            begin = SubElement(timespan, 'begin')
+            begin.text = unicode(feature['properties']['valid']['from'].toString("yyyy-MM-ddThh:mm:ssZ"))
+            end = SubElement(timespan, 'end')
+            end.text = unicode(feature['properties']['valid']['to'].toString("yyyy-MM-ddThh:mm:ssZ"))
+            
+            polygons = feature['geometry']['coordinates']
+            for coords in polygons:
+            
+                placemark = SubElement(folder, 'Placemark')
+                SubElement(placemark, 'name').text = feature['properties']['parameterGroup']
+                SubElement(placemark, 'description').text = ''
+                
+                extdata = SubElement(placemark, 'ExtendedData')
+                data = SubElement(extdata, 'Data')
+                data.set('name', u'met:objectType')
+                SubElement(data, 'value').text = 'PolyLine'
+                
+                polygon = SubElement(placemark, 'Polygon')
+                SubElement(polygon, 'tessellate').text = '1'
+                
+                boundary = SubElement(polygon, 'outerBoundaryIs')
+                ring = SubElement(boundary, 'LinearRing')
+                coordinates = SubElement(ring, 'coordinates')
+                text = u''
+                
+                for coord in coords:
+                    line = u"%f,%f,0\n" % (coord.x(), coord.y())
+                    text += line
+                
+                coordinates.text = text
+    
+    if kml_file == '-':
+        f = sys.stdout
+    else:
+        f = open(kml_file, 'wb')
+    
+    ElementTree(kml).write(f, 'utf-8', '<?xml version="1.0" encoding="UTF-8"?>')
